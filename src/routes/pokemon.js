e1f6472364f961671e42d738b88989e941a88406
Feber Castellon <cochabambinoski@gmail.com>
2022-03-24 06:40:05 -0400

    values and type validation and image downloader

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -2,19 +2,28 @@ const {Router} = require('express');
 const router = Router();
 const fetch = require('node-fetch');
 
-const thisPoke = {}
+const download = require('../image-download')
 
 const getPokemon = async (pokemonName) => {
     let url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`    
     const response = await fetch(url);
     const data = await response.json()
-    console.log(data)
+    console.log(data)    
     return data
 }
 
-router.get(`/api/pokemon/:pokemon`,(req,res) => {
-    const s = getPokemon(req.params.pokemon);
-    res.json(s);
+router.get(`/api/pokemon/:pokemon&:itype`,(req,res) => {
+    if(req.params.itype==="front_default" || req.params.itype==="front_shiny"){
+        const s = getPokemon(req.params.pokemon);
+        download(s.sprites.front_default,req.params.pokemon + "_"+req.params.itype,function(){
+            console.log("image saved")
+        });
+        res.json(s);
+
+    } else {
+        res.send("Incorrect type");
+    }
+
 });
 
 module.exports = router;
