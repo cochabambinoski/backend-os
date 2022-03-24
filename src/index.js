017ad344db36aee16547dae8d892b00ffdc7d4f4
Feber Castellon <cochabambinoski@gmail.com>
2022-03-23 23:26:48 -0400

    repository initialized

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+const morgan = require('morgan');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'));
+app.use('/api/pokemon',require('./routes/pokemon'));
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+});
